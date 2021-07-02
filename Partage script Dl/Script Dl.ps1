435a021b6e5c31159701d723d24fab2036c3402f
Jtejure <86807532+Jtejure@users.noreply.github.com>
2021-07-02 07:07:16 +0200

    Add files via upload

---
+++ Partage script Dl/Script Dl.ps1
@@ -0,0 +1,46 @@
+﻿# SECURISATION RESSOURCES WINDOWS SERVER 2008
+
+
+
+
+# Emplacement du fichier de création de l'arbo
+$fic = "E:\Scripts\arbo_NTFS.txt"
+
+# Récupération du SID pour le CREATEUR PROPRIETAIRE
+$CP = New-Object System.Security.Principal.NtAccount("CREATEUR PROPRIETAIRE")
+$CPsid = $CP.Translate([System.Security.Principal.SecurityIdentifier])
+
+# Récupération du SID pour le compte SYSTEM
+$ST = New-Object System.Security.Principal.NtAccount("SYSTEM")
+$STsid = $ST.Translate([System.Security.Principal.SecurityIdentifier])
+
+# Création de l'UO DL dans le domaine
+New-ADOrganizationalUnit -Name DL
+
+# Stockage du DN de l'UO DL
+$dom = Get-ADDomain
+$DN = "OU=DL," + $dom.DistinguishedName
+
+# Stockage du nom de domaine NETBIOS
+$domaine = $dom.NetBIOSName
+ 
+# Récupération du contenu du fichier d'import
+$liste = Get-Content -Path $fic
+
+Foreach ($dossier in $liste)
+{
+	# Récupération du nom relatif du dossier
+	$lignes = $dossier.Split("\") | Measure-Object -Line
+	$pos = $lignes.Lines
+	$rep = $dossier.Split("\")[$pos-1]
+
+	# Création des DL relatives au nom de dossier
+	$nom = "DL_"+$rep
+	New-ADGroup -Name $nom"_R" -GroupScope DomainLocal -Path $DN
+	New-ADGroup -Name $nom"_RW" -GroupScope DomainLocal -Path $DN
+
+	# Création des dossiers avec chemin complet
+	New-Item -Type directory -Path $dossier
+
+	
+}
---
+++ Partage script Dl/scripts_creation_DL.ps1
@@ -0,0 +1,29 @@
+﻿
+$dom = Get-ADDomain
+$DN = "OU=DL_TEST," + $dom.DistinguishedName
+
+
+echo "Entrez le chemin absolu : "
+$path = Read-Host 
+$arborescence = Get-ChildItem -Path  $path -Recurse
+$arborescence.FullName
+
+
+New-ADOrganizationalUnit -Name DL_Test -Path "DC=zbr,DC=intradef,DC=gouv,DC=fr"
+
+
+
+
+$dom = Get-ADDomain
+$DN = "OU=DL_TEST," + $dom.DistinguishedName
+
+
+
+
+Foreach ($dossier in $arborescence)
+{
+	# Création des DL relatives au nom de dossier
+	$nom = "DL_"+ $dossier
+	New-ADGroup -Name $nom"_R" -GroupScope DomainLocal -Path $DN
+	New-ADGroup -Name $nom"_RW" -GroupScope DomainLocal -Path $DN	
+}
